88399d3585c6fdff2bee61188d615b15c1ca8d1e
Christopher Giroux <cgiroux86@gmail.com>
2020-01-31 12:46:26 -0500

    Changed code on unisWithUni

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -85,7 +85,7 @@ console.log(contactInfo)
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-let unisWithUni = graduates.filter(elem => elem.university.includes('Uni')).map(elem => elem.university)
+let unisWithUni = graduates.filter(elem => elem.university.includes('Uni'))//.map(elem => elem.university)
 console.log(unisWithUni)
 
 
